121f5045f8f7c2f6f43f7f05effc6629340e04c6
gebrina <gsonofmika@gmail.com>
2023-09-26 17:14:06 +0300

    added jwt-startegy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { User } from "src/interfaces/user.interface";
 import { UserService } from "src/user/user.service";
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, password: string) {
     const user = await this.userService.findUserByName(username);
@@ -12,11 +16,15 @@ export class AuthService {
     return null;
   }
 
-  async login(user: User): Promise<User | undefined> {
-    const savedUser = await this.userService.findUserByName(user.username);
-    if (savedUser?.password !== user.password)
+  async login(user: User): Promise<any> {
+    const { username, password } = user;
+    const saveduser = await this.userService.findUserByName(username);
+    if (saveduser?.password !== password)
       throw new UnauthorizedException("Invalid username or password");
 
-    return user;
+    const { password: userPassword, ...result } = saveduser;
+    return {
+      access_token: await this.jwtService.signAsync(result),
+    };
   }
 }
