0f4099236b23d93feff3543d20454055a920b33d
gebrina <gsonofmika@gmail.com>
2023-09-28 15:28:04 +0300

    updated product entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "sqlite",
       database: "./database.sqlite",
-      entities: ["/dist/**/*.entity{.ts,.js}"],
+      entities: ["dist/**/*.entity{.ts,.js}"],
       autoLoadEntities: true,
       synchronize: true,
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Product } from "./product";
+import { Product } from "./product.entity";
 
 @Entity()
 export class User {
