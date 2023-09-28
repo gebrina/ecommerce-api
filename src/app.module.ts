3f6558be7facdc222bd6aa0ed44bd812316b55d6
gebrina <gsonofmika@gmail.com>
2023-09-28 14:59:46 +0300

    project created and added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,4 @@
 import { Module } from "@nestjs/common";
-import { UserModule } from "./user/user.module";
-import { PassportModule } from "@nestjs/passport";
-import { AuthModule } from "./auth/auth.module";
 
-@Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: "jwt", session: false }),
-    AuthModule,
-    UserModule,
-  ],
-})
+@Module({})
 export class AppModule {}
