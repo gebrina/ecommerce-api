ff4b120f2f8ce57cdf8a96850a3e68f1739b35dd
gebrina <gsonofmika@gmail.com>
2023-10-01 23:00:14 +0300

    updated create user api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,6 +9,7 @@ import {
 } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { User } from "src/entities/user.entity";
+import { Public } from "src/decorators/PublicApi.decrator";
 
 @Controller("users")
 export class UserController {
@@ -19,18 +20,13 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  /**
-   * Find one user by ID.
-   *
-   * @param {string} id - The ID of the user.
-   * @return {Promise<User>} A promise that resolves to the user with the given ID.
-   */
   @Get(":id")
   findOne(@Param("id") id: string): Promise<User> {
     return this.userService.findOne(id);
   }
 
   @Post()
+  @Public()
   create(@Body() user: User): Promise<User> {
     return this.userService.create(user);
   }
