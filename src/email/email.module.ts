7b7522c4b13bfdb2e9727a4847fffccc299f3d12
gebrina <gsonofmika@gmail.com>
2023-10-29 13:37:27 +0300

    added email config to  email module

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,16 +1,37 @@
 import { Module, Global } from "@nestjs/common";
-import { MailerModule, MailerService } from "@nestjs-modules/mailer";
+import { MailerModule } from "@nestjs-modules/mailer";
 import { EjsAdapter } from "@nestjs-modules/mailer/dist/adapters/ejs.adapter";
 import { ConfigService } from "@nestjs/config";
+import { join } from "path";
+import { EmailService } from "./email.service";
 
 @Global()
 @Module({
   imports: [
     MailerModule.forRootAsync({
-      useFactory: (config: ConfigService) => ({}),
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get("SMTP_HOST"), // email protocol e.g if you use gmail user smtp.gmail.com
+          auth: {
+            user: config.get("SMTP_USERNAME"), // username@email.com
+            pass: config.get("SMTP_PASSWORD"), //app password
+          },
+        },
+        defaults: {
+          from: "<no-reply> G> tEcHnOlOGy",
+        },
+        template: {
+          adapter: new EjsAdapter(),
+          dir: join(__dirname, "templates"),
+          options: {
+            strict: false,
+          },
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
-  providers: [MailerService],
-  exports: [MailerService],
+  providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {}
